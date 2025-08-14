d8fae33b41070586d9447466162c8f836102fc30
iTzTaisn69 <itztasinwiki@gmail.com>
2025-08-14 22:29:52 +0600

    Update domin.mk

--- domin.mk
+++ domin.mk
@@ -18,19 +18,7 @@ ascii_art="
 
 "
 
-apt install github
-
-  # Colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
-
-
 echo -e "${GREEN}$ascii_art${NC}"
-
 
 apt update && curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
@@ -42,4 +30,12 @@ apt update && curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 ngrok config add-authtoken 2ydA4rZ4zjoGV2Mt9TnmMwtkupp_5fW7zp6M7WWtxdKyWFE8B
 
 ngrok http http://localhost:8030
+
+  # Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
