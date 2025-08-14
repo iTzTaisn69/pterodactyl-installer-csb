d5aa6d2dec6da97c8aecdde9068bdb3fe07315bc
iTzTaisn69 <itztasinwiki@gmail.com>
2025-08-14 22:30:52 +0600

    Update domin.mk

--- domin.mk
+++ domin.mk
@@ -1,4 +1,10 @@
-# its only forword ptero csb panel port and its use ngrok 
+  # Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
 
 # ASCII Art
@@ -25,17 +31,8 @@ apt update && curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
   && sudo apt update \
-  && sudo apt install ngrok
+  && sudo apt install ngrok -y
 
 ngrok config add-authtoken 2ydA4rZ4zjoGV2Mt9TnmMwtkupp_5fW7zp6M7WWtxdKyWFE8B
 
 ngrok http http://localhost:8030
-
-  # Colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
-
